3cf380c691f4fb11bfab660d4a97cea2174d3bab
Bas <bas-can@hotmail.com>
2021-07-25 21:14:59 +0200

    Edit package settings

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,22 +1,16 @@
-import replace from '@rollup/plugin-replace';
-import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
-import { terser } from 'rollup-plugin-terser';
+import replace from "@rollup/plugin-replace";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
 
 export default [{
 	// Library packaging
-	input: './src/index.ts',
-	output: [
-		{
-			file: './dist/index.js',
-			format: 'es',
-		},
-		{
-			file: './dist/iife/openrct2-mocks.js',
-			format: 'iife',
-			name: 'mock'
-		}
-	],
+	input: "./src/index.ts",
+	output: {
+		file: "./dist/index.js",
+		format: "cjs",
+		exports: "default"
+	},
 	plugins: [
 		replace({
 			include: "./src/utilities/environment.ts",
@@ -25,13 +19,13 @@ export default [{
 				__BUILD_CONFIGURATION__: JSON.stringify("development")
 			}
 		}),
-		typescript({ tsconfig: './tsconfig.json' }),
+		typescript({ tsconfig: "./tsconfig.json" }),
 		terser({
 			format: {
 				comments: false,
 				quote_style: 1,
 				wrap_iife: true,
-				preamble: '// Get the latest version: https://github.com/Basssiiie/OpenRCT2-Mocks',
+				preamble: "// Get the latest version: https://github.com/Basssiiie/OpenRCT2-Mocks",
 
 				beautify: true,
 			},
@@ -43,13 +37,13 @@ export default [{
 },
 {
 	// Declaration file packaging
-	input: './src/index.ts',
+	input: "./src/index.ts",
 	output: {
-		file: './dist/index.d.ts',
-		format: 'es',
+		file: "./dist/index.d.ts",
+		format: "es",
 	},
 	plugins: [
-		typescript({ tsconfig: './tsconfig.dts.json' }),
+		typescript({ tsconfig: "./tsconfig.dts.json" }),
 		dts()
 	]
 }];
--- rollup.config.prod.js
+++ rollup.config.prod.js
@@ -1,22 +1,16 @@
-import replace from '@rollup/plugin-replace';
-import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
-import { terser } from 'rollup-plugin-terser';
+import replace from "@rollup/plugin-replace";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
 
 export default [{
 	// Library packaging
-	input: './src/index.ts',
-	output: [
-		{
-			file: './dist/index.min.js',
-			format: 'es',
-		},
-		{
-			file: './dist/iife/openrct2-mocks.min.js',
-			format: 'iife',
-			name: 'mock'
-		}
-	],
+	input: "./src/index.ts",
+	output: {
+		file: "./dist/index.min.js",
+		format: "cjs",
+		exports: "default"
+	},
 	plugins: [
 		replace({
 			include: "./src/utilities/environment.ts",
@@ -25,13 +19,13 @@ export default [{
 				__BUILD_CONFIGURATION__: JSON.stringify("production")
 			}
 		}),
-		typescript({ tsconfig: './tsconfig.json' }),
+		typescript({ tsconfig: "./tsconfig.json" }),
 		terser({
 			format: {
 				comments: false,
 				quote_style: 1,
 				wrap_iife: true,
-				preamble: '// Get the latest version: https://github.com/Basssiiie/OpenRCT2-Mocks',
+				preamble: "// Get the latest version: https://github.com/Basssiiie/OpenRCT2-Mocks",
 			},
 			mangle: {
 				properties: {
@@ -43,13 +37,13 @@ export default [{
 },
 {
 	// Declaration file packaging
-	input: './src/index.ts',
+	input: "./src/index.ts",
 	output: {
-		file: './dist/index.d.ts',
-		format: 'es',
+		file: "./dist/index.d.ts",
+		format: "es",
 	},
 	plugins: [
-		typescript({ tsconfig: './tsconfig.dts.json' }),
+		typescript({ tsconfig: "./tsconfig.dts.json" }),
 		dts()
 	]
 }];
